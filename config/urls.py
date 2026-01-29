895d8ec4de684e61198df849cce0f191be31b20a
braddy123-bit <braddylondre123@gmail.com>
2026-01-28 22:35:20 -0500

    Create urls.py

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for hoja_vida_project project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = get_wsgi_application()
