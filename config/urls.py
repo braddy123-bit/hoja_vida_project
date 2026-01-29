49b9ed2ef2c336703f60aabccc5c801b53627dce
braddy123-bit <braddylondre123@gmail.com>
2026-01-28 22:35:36 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,22 @@
 """
-WSGI config for hoja_vida_project project.
+URL configuration for hoja_vida_project project.
 """
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-import os
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('apps.core.urls')),
+    path('perfil/', include('apps.perfiles.urls')),
+]
 
-from django.core.wsgi import get_wsgi_application
+# Configuración del admin
+admin.site.site_header = "Administración Hoja de Vida"
+admin.site.site_title = "Panel Administrativo"
+admin.site.index_title = "Gestión de Hojas de Vida"
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-application = get_wsgi_application()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
